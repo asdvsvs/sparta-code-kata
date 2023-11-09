b13dfb97c9189e9da0731a5dee32fb51c50045ee
asdvsvs <gag973000@gmail.com>
2023-11-09 16:08:54 +0900

    StrangeCharacters

---
+++ src/main/java/org/sparta/programmers/lv3/StrangeCharacters.java
@@ -0,0 +1,21 @@
+package org.sparta.programmers.lv3;
+
+public class StrangeCharacters {
+    public static void main(String[] args) {
+        class Solution {
+            public String solution(String s) {
+                char[] upStr = s.toUpperCase().toCharArray();
+                char[] lowStr = s.toLowerCase().toCharArray();
+                int cnt=0;
+                for (int i=0;i<s.length();i++){
+                    if(upStr[i]==' ') cnt=0;
+                    else {
+                        if(cnt%2==1) upStr[i] = lowStr[i];
+                        cnt++;
+                    }
+                }
+                return new String(upStr);
+            }
+        }
+    }
+}
