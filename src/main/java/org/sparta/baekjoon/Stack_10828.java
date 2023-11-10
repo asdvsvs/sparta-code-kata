95c591aec6ddbed479975188d85422c9840941e6
asdvsvs <gag973000@gmail.com>
2023-11-10 21:22:51 +0900

    stack_10828

---
+++ src/main/java/org/sparta/baekjoon/Stack_10828.java
@@ -0,0 +1,49 @@
+package org.sparta.baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+// 스캐너는 시간초과
+// 버퍼리더를 통과
+// 앞으로 버퍼리더를 사용하자
+public class Stack_10828 {
+    public static void main(String[] args) throws IOException {
+        Stack<Integer> stack = new Stack<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            st =  new StringTokenizer(br.readLine());
+            String text = st.nextToken();
+            switch (text) {
+                case "push":
+                    stack.push(Integer.parseInt(st.nextToken()));
+                    break;
+                case "pop":
+                    if (stack.empty()) {
+                        System.out.println(-1);
+                        break;
+                    }
+                    System.out.println(stack.pop());
+                    break;
+                case "size":
+                    System.out.println(stack.size());
+                    break;
+                case "empty":
+                    if (stack.empty()) System.out.println(1);
+                    else System.out.println(0);
+                    break;
+                case "top":
+                    if (stack.empty()) {
+                        System.out.println(-1);
+                        break;
+                    }
+                    System.out.println(stack.peek());
+                    break;
+            }
+        }
+    }
+}
