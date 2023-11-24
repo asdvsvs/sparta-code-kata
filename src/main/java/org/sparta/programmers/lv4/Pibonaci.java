7e50de8f41bd5f34c22234aeab80b32202a30be0
asdvsvs <gag973000@gmail.com>
2023-11-24 09:59:40 +0900

    23.11.24 code kata

---
+++ src/main/java/org/sparta/programmers/lv4/Pibonaci.java
@@ -0,0 +1,20 @@
+package org.sparta.programmers.lv4;
+
+public class Pibonaci {
+    public static void main(String[] args) {
+        class Solution {
+            public int solution(int n) {
+                int answer = 0;
+                int[] pibonaci = new int [n+1];
+                pibonaci[0] =0;
+                pibonaci[1] =1;
+                for(int i=2; i<=n;i++){
+                    pibonaci[i]= (pibonaci[i-1]+pibonaci[i-2])%1234567;
+                }
+                answer=pibonaci[n];
+
+                return answer;
+            }
+        }
+    }
+}
