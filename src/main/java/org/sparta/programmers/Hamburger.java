a35bee2495a170b53cd7e315657586a830b03274
asdvsvs <gag973000@gmail.com>
2023-12-04 04:19:25 +0900

    23.12.03 code kata

--- src/main/java/org/sparta/programmers/Hamburger.java
+++ src/main/java/org/sparta/programmers/Hamburger.java
@@ -3,30 +3,35 @@ package org.sparta.programmers;
 import java.util.ArrayList;
 import java.util.List;
 
-class Solution {
-    public int solution(int[] ingredient) {
-        int answer = 0;
-        int temp =0;
-        List<Integer> ingreList = new ArrayList<>();
-        for(int i:ingredient) ingreList.add(i);
+public class Hamburger {
+    public static void main(String[] args) {
+        class Solution {
+            public int solution(int[] ingredient) {
+                int answer = 0;
+                int temp =0;
+                List<Integer> ingreList = new ArrayList<>();
+                for(int i:ingredient) ingreList.add(i);
 
-        while(true){
-            for(int i=0;i<ingreList.size()-3;i++){
-                if(ingreList.get(i)==1
-                        && ingreList.get(i+1)==2
-                        && ingreList.get(i+2)==3
-                        && ingreList.get(i+3)==1
-                ){
-                    answer++;
-                    for(int j=0;j<4;j++)ingreList.remove(i);
-                    i-=3;
-                    if(i<0)i=0;
+                while(true){
+                    for(int i=0;i<ingreList.size()-3;i++){
+                        if(ingreList.get(i)==1
+                                && ingreList.get(i+1)==2
+                                && ingreList.get(i+2)==3
+                                && ingreList.get(i+3)==1
+                        ){
+                            answer++;
+                            for(int j=0;j<4;j++)ingreList.remove(i);
+                            i-=3;
+                            if(i<0)i=0;
+                        }
+                    }
+                    if(temp==answer)break;
+                    else temp=answer;
                 }
-            }
-            if(temp==answer)break;
-            else temp=answer;
-        }
 
-        return answer;
+                return answer;
+            }
+        }
     }
 }
+
