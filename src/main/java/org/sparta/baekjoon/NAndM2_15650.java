c26cdc1e04d628178e49ce4a4939e89dae2d1a95
asdvsvs <gag973000@gmail.com>
2023-12-05 17:44:18 +0900

    23.12.05 code kata

---
+++ src/main/java/org/sparta/baekjoon/NAndM2_15650.java
@@ -0,0 +1,40 @@
+package org.sparta.baekjoon;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class NAndM2_15650 {
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        function(n, m, 0, "");
+
+        bw.write(String.valueOf(sb));
+        bw.flush();
+        bw.close();
+    }
+
+    public static void function(int n, int m, int length, String s) {
+        if (length >= m) {
+            sb.append(s.trim()).append("\n");
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (!s.contains(String.valueOf(i))) {
+                if(!s.isEmpty()){
+                    String temp=s.substring(s.length()-1);
+                    if(Integer.parseInt(temp)<i) function(n, m, length + 1, s + " " + i);
+                }
+                else function(n, m, length + 1, s + " " + i);
+            }
+        }
+    }
+}
