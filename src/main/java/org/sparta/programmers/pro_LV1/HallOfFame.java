370acdd3fcdfbd6b9e6d949da4b97e9e9c7a8c83
asdvsvs <gag973000@gmail.com>
2024-02-22 18:10:23 +0900

    24.2.22 code kata

---
+++ src/main/java/org/sparta/programmers/pro_LV1/HallOfFame.java
@@ -0,0 +1,31 @@
+package org.sparta.programmers.pro_LV1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class HallOfFame {
+
+    public static void main(String[] args) {
+        class Solution {
+
+            public int[] solution(int k, int[] score) {
+                int[] answer = new int[score.length];
+
+                List<Integer> list = new ArrayList<>();
+
+                for (int i = 0; i < score.length; i++) {
+                    list.add(score[i]);
+                    Collections.sort(list);
+                    if (list.size() < k) {
+                        answer[i] = list.get(0);
+                    } else {
+                        answer[i] = list.get(list.size() - k);
+                    }
+                }
+
+                return answer;
+            }
+        }
+    }
+}
