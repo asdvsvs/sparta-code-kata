4d8700deb36b2946a6e5f73828293d4a934f0e72
asdvsvs <gag973000@gmail.com>
2024-04-01 12:00:37 +0900

    24.4.1 code kata

---
+++ src/main/java/org/sparta/programmers/pro_LV2/Fibonacci.java
@@ -0,0 +1,20 @@
+package org.sparta.programmers.pro_LV2;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/12945
+public class Fibonacci {
+    public static void main(String[] args) {
+        class Solution {
+            public int solution(int n) {
+                int answer = 0;
+                int[] arr = new int[n + 1];
+                arr[0] = 0;
+                arr[1] = 1;
+                for (int i = 2; i <= n; i++) {
+                    arr[i] = (arr[i - 1] + arr[i - 2]) % 1234567;
+                }
+                answer = arr[n];
+                return answer;
+            }
+        }
+    }
+}
