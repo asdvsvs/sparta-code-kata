9a6ffc1d9d614cab4e4f8659ffb1f1d2d30de512
asdvsvs <gag973000@gmail.com>
2024-04-02 12:11:37 +0900

    24.4.2 code kata

---
+++ src/main/java/org/sparta/programmers/pro_LV0/Pro_181947.java
@@ -0,0 +1,20 @@
+package org.sparta.programmers.pro_LV0;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/181947
+
+import java.util.Scanner;
+
+public class Pro_181947 {
+    public static void main(String[] args) {
+
+        class Solution {
+            public static void main(String[] args) {
+                Scanner sc = new Scanner(System.in);
+                int a = sc.nextInt();
+                int b = sc.nextInt();
+
+                System.out.println(a + " + " + b + " = " + (a + b));
+            }
+        }
+    }
+}
