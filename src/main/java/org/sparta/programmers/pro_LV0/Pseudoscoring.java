1369bce918fd1aa475f25b4ec41ae063d8fa8e54
asdvsvs <gag973000@gmail.com>
2024-04-02 12:08:36 +0900

    24.4.2 code kata

---
+++ src/main/java/org/sparta/programmers/pro_LV0/Pseudoscoring.java
@@ -0,0 +1,24 @@
+package org.sparta.programmers.pro_LV0;
+
+//https://school.programmers.co.kr/learn/courses/30/lessons/250128
+public class Pseudoscoring {
+    public static void main(String[] args) {
+        class Solution {
+            public String[] solution(int[] numbers, int[] our_score, int[] score_list) {
+                int num_student = numbers.length;
+                String[] answer = new String[num_student];
+
+                for (int i = 0; i < num_student; i++) {
+                    if (our_score[i] == score_list[numbers[i] - 1]) {
+                        answer[i] = "Same";
+                    } else {
+                        answer[i] = "Different";
+                    }
+                }
+
+                return answer;
+            }
+        }
+
+    }
+}
