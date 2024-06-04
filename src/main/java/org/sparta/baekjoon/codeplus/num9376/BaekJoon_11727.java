70151ff94e4fd9ede6e9c9280e25b30af472a9ba
asdvsvs <gag973000@gmail.com>
2024-06-04 19:35:14 +0900

    24.6.4 code kata

---
+++ src/main/java/org/sparta/baekjoon/codeplus/num9376/BaekJoon_11727.java
@@ -0,0 +1,25 @@
+package org.sparta.baekjoon.codeplus.num9376;
+
+import java.io.*;
+
+public class BaekJoon_11727 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n + 1];
+
+        arr[1] = 1;
+        if (n > 1) {
+            arr[2] = 3;
+        }
+        for (int i = 3; i <= n; i++) {
+            arr[i] = (arr[i - 1] + (arr[i - 2] * 2)) % 10007;
+        }
+
+        bw.write(arr[n] + "");
+        bw.flush();
+        bw.close();
+    }
+}
